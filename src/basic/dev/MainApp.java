3c5242a8cd0d0211828ea8b83187cd79a4599524
Man <tranman731@gmail.com>
2021-10-25 14:37:52 +0700

    first commit

---
+++ src/basic/dev/MainApp.java
@@ -0,0 +1,35 @@
+package basic.dev;
+/**
+ * Ghi chú ( comment nhiều dòng )
+ * @author Acer
+ * 1. Tên lớp trùng tên file
+ * 2. Tên lớp chữ cái đầu viết hoa . Nếu từ ghép thì những chữ cái đầu viết hoa
+ * 4. Tên lớp đặt phải có ý nghĩa . Ví dụ : class Peson : lớp mô tả con người 
+ * - Interface : Phải có tiền tố I . Ví dụ : Ilistener
+ * - Abstract  : Phải có tiền tố Abstract . Ví dụ : AbstractShape
+ */
+
+public class MainApp {
+       //Comment 1 dòng cách 1 dòng . Phím nóng ctrl + /
+       /*Comment 1 dòng cách 2*/
+       public static void main(String[] args) {
+    	   System.out.println("Hello World");
+    	   /**
+    	    * Cách đặt tên biên :
+    	    * 1. Không chứa kí tự đặc biệt @*#(@#
+    	    * 2. Không chứa khoảng trắng 
+    	    * 3. Viết thường 
+    	    * 4. Không được bắt đầu bằng số 
+    	    */
+    	   int number;
+    	   String nameSchool;
+           }
+    	   /*
+    	    * tên phương thức tuân theo quy tắc camelCase
+    	    */
+    	   public void show() {
+		// TODO Auto-generated method stub
+
+	}
+
+}
